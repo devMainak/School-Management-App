8508b0ca1d2e11406151d2f24ed37c31690f9f33
rootKid <137388933+devMainak@users.noreply.github.com>
2024-07-26 08:28:28 +0000

    feat: fetchStudents

--- src/features/students/studentsSlice.js
+++ src/features/students/studentsSlice.js
@@ -1,4 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const fetchStudents = createAsyncThunk("fetch/students", async () => {
+  const response = await axios.get("https://06cae953-318f-4749-845b-a59b064b4f4b-00-33u2c47zy1ymn.pike.replit.dev/students")
+
+  return response
+})
 
 export const studentsSlice = createSlice({
   name: "students",
@@ -7,7 +14,22 @@ export const studentsSlice = createSlice({
     status: "idle",
     error: null
   },
-  reducers: {}
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchStudents.pending, (state) => {
+      state.status = "loading"
+    })
+
+    builder.addCase(fetchStudents.fulfilled, (state, action) => {
+      state.status = "success"
+      state.students = action.payload
+    })
+
+    builder.addCase(fetchStudents.rejected, (state, action) => {
+      state.status = "error"
+      state.error = action.payload.error
+    })
+  }
 })
 
 export default studentsSlice.reducer
